af760ce1b9465a992fa917462f3c07175eb282d8
Dinar <dinar095@hotmail.com>
2021-06-06 11:46:32 +0300

    add crashed wp

--- sql/srcs/start.sh
+++ sql/srcs/start.sh
@@ -5,12 +5,8 @@ sed -i "s|.*skip-networking.*|#skip-networking|g" /etc/my.cnf.d/mariadb-server.c
 PASSWDDB='1234'
 MAINDB='wordpress'
 USER='admin'
-
 mysql -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 mysql -e "CREATE USER $USER@'' IDENTIFIED BY '${PASSWDDB}';"
 mysql -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${USER}'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
 mysqld_safe
-
-
-
