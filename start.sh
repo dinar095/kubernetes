f3834e7aa673e137d375218140ed939ed310358b
Desausag <dinar095@hotmail.com>
2021-06-15 16:46:16 +0300

    correct mysql

--- start.sh
+++ start.sh
@@ -3,10 +3,13 @@ minikube delete
 minikube start --driver=virtualbox --addons=metallb
 eval $(minikube -p minikube docker-env)
 docker build . -t alpine_local
+docker build sql/. -t sql
 docker build ftps/. -t ftps
+docker build nginx/. -t nginx
+docker build wordpress/. -t wordpress
+docker build phpmyadmin/. -t phpmyadmin
 docker pull metallb/speaker:v0.8.2
 docker pull metallb/controller:v0.8.2
-docker build nginx/. -t nginx
 kubectl apply -f configmap.yaml
 kubectl apply -f nginx/nginx.yaml
 kubectl apply -f ftps/ftps.yaml
